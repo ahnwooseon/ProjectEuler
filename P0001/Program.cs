69fafdad4ca781d4a28e24397cc233c02f2fd1cf
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-06 10:17:51 +0000

    P0001

---
+++ P0001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine($"{SumOfMultiples(3, 1000) + SumOfMultiples(5, 1000) - SumOfMultiples(15, 1000)}");
+
+int SumOfMultiples(int n, int exclusiveUpperLimit)
+{
+    int biggestFactor = (exclusiveUpperLimit - 1) / n;
+
+    return n * biggestFactor * (biggestFactor + 1) / 2;
+}
