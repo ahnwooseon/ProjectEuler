2285b450adea0abe95247a79b4b47ba08e64cbb0
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-07 02:56:13 +0000

    P0002

---
+++ P0002/Program.cs
@@ -0,0 +1,22 @@
+﻿// foreach (int i in Fibonacci(4_000_000))
+// {
+//     Console.WriteLine($"{i}");
+// }
+
+Console.WriteLine($"{Fibonacci(4_000_000).Where(x => x % 2 == 0).Sum()}");
+
+IEnumerable<int> Fibonacci(int limit)
+{
+    yield return 1;
+    yield return 2;
+
+    int a = 1;
+    int b = 2;
+    while (a + b < limit)
+    {
+        int c = a + b;
+        yield return c;
+        a = b;
+        b = c; 
+    }
+}
